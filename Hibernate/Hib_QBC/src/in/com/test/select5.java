01b11efb3d85a84d2bd16ed781c8edca0f017765
shashank <shashank.raj@gmail.com>
2023-04-19 03:52:00 +0530

    added in master

--- Hibernate/Hib_QBC/src/in/com/test/select5.java
+++ Hibernate/Hib_QBC/src/in/com/test/select5.java
@@ -52,6 +52,7 @@ public class select5 {
                          System.out.println();
                          System.out.println();
                          System.out.println();
+                         System.out.println();
                      }
     		);
 		 }		
